4afff4913d6105e353e7a1da9dac4fedb58b29b4
Alexis <alexis.billietet32@gmail.com>
2026-05-09 18:43:49 -0300

    Add files via upload

---
+++ Actividad/tp_escrituras1.ino
@@ -0,0 +1,227 @@
+#define buzz 11
+#define rojo1 3
+#define verde1 9
+#define rojo2 4
+#define azul2 7
+#define verde2 10
+#define azul3 5
+#define verde3 6
+#define rojo3 12
+void setup()
+{
+ pinMode(buzz, OUTPUT);
+ pinMode(rojo1, OUTPUT);
+ pinMode(verde1, OUTPUT);
+ pinMode(rojo2, OUTPUT);
+ pinMode(azul2, OUTPUT);
+ pinMode(verde2, OUTPUT);
+ pinMode(azul3, OUTPUT);
+ pinMode(verde3, OUTPUT);
+ pinMode(rojo3, OUTPUT);
+}
+
+void loop()
+{
+  analogWrite(rojo1, 255);
+  delay(500);
+  analogWrite(rojo1, 0);
+  
+  //
+  
+  digitalWrite(rojo1, HIGH);
+  analogWrite(verde1, 60);
+  digitalWrite(rojo2, HIGH);
+  analogWrite(verde2, 60);
+  digitalWrite(rojo3, HIGH);
+  analogWrite(verde3, 60);
+  delay(100);
+  digitalWrite(rojo1, LOW);
+  analogWrite(verde1, 0);
+  digitalWrite(rojo2, LOW);
+  analogWrite(verde2, 0);
+  digitalWrite(rojo3, LOW);
+  analogWrite(verde3, 0);
+  analogWrite(buzz, 255);
+  delay(100);
+  analogWrite(buzz, 0);
+  digitalWrite(rojo1, HIGH);
+  analogWrite(verde1, 60);
+  digitalWrite(rojo2, HIGH);
+  analogWrite(verde2, 60);
+  digitalWrite(rojo3, HIGH);
+  analogWrite(verde3, 60);
+  delay(100);
+  digitalWrite(rojo1, LOW);
+  analogWrite(verde1, 0);
+  digitalWrite(rojo2, LOW);
+  analogWrite(verde2, 0);
+  digitalWrite(rojo3, LOW);
+  analogWrite(verde3, 0);
+  delay(100);
+  digitalWrite(rojo1, HIGH);
+  analogWrite(verde1, 60);
+  digitalWrite(rojo2, HIGH);
+  analogWrite(verde2, 60);
+  digitalWrite(rojo3, HIGH);
+  analogWrite(verde3, 60);
+  delay(100);
+  digitalWrite(rojo1, LOW);
+  analogWrite(verde1, 0);
+  digitalWrite(rojo2, LOW);
+  analogWrite(verde2, 0);
+  digitalWrite(rojo3, LOW);
+  analogWrite(verde3, 0);
+  delay(100);
+  digitalWrite(rojo1, HIGH);
+  analogWrite(verde1, 60);
+  digitalWrite(rojo2, HIGH);
+  analogWrite(verde2, 60);
+  digitalWrite(rojo3, HIGH);
+  analogWrite(verde3, 60);
+  delay(100);
+  digitalWrite(rojo1, LOW);
+  analogWrite(verde1, 0);
+  digitalWrite(rojo2, LOW);
+  analogWrite(verde2, 0);
+  digitalWrite(rojo3, LOW);
+  analogWrite(verde3, 0);
+  
+  //
+  
+  digitalWrite(rojo2, HIGH);
+  digitalWrite(azul2, HIGH);
+  delay(500);
+  digitalWrite(rojo2, LOW);
+  digitalWrite(azul2, LOW);
+  
+  //
+  
+  digitalWrite(rojo1, HIGH);
+  analogWrite(verde1, 60);
+  digitalWrite(rojo2, HIGH);
+  analogWrite(verde2, 60);
+  digitalWrite(rojo3, HIGH);
+  analogWrite(verde3, 60);
+  delay(100);
+  digitalWrite(rojo1, LOW);
+  analogWrite(verde1, 0);
+  digitalWrite(rojo2, LOW);
+  analogWrite(verde2, 0);
+  digitalWrite(rojo3, LOW);
+  analogWrite(verde3, 0);
+  analogWrite(buzz, 255);
+  delay(100);
+  analogWrite(buzz, 0);
+  digitalWrite(rojo1, HIGH);
+  analogWrite(verde1, 60);
+  digitalWrite(rojo2, HIGH);
+  analogWrite(verde2, 60);
+  digitalWrite(rojo3, HIGH);
+  analogWrite(verde3, 60);
+  delay(100);
+  digitalWrite(rojo1, LOW);
+  analogWrite(verde1, 0);
+  digitalWrite(rojo2, LOW);
+  analogWrite(verde2, 0);
+  digitalWrite(rojo3, LOW);
+  analogWrite(verde3, 0);
+  delay(100);
+  digitalWrite(rojo1, HIGH);
+  analogWrite(verde1, 60);
+  digitalWrite(rojo2, HIGH);
+  analogWrite(verde2, 60);
+  digitalWrite(rojo3, HIGH);
+  analogWrite(verde3, 60);
+  delay(100);
+  digitalWrite(rojo1, LOW);
+  analogWrite(verde1, 0);
+  digitalWrite(rojo2, LOW);
+  analogWrite(verde2, 0);
+  digitalWrite(rojo3, LOW);
+  analogWrite(verde3, 0);
+  delay(100);
+  digitalWrite(rojo1, HIGH);
+  analogWrite(verde1, 60);
+  digitalWrite(rojo2, HIGH);
+  analogWrite(verde2, 60);
+  digitalWrite(rojo3, HIGH);
+  analogWrite(verde3, 60);
+  delay(100);
+  digitalWrite(rojo1, LOW);
+  analogWrite(verde1, 0);
+  digitalWrite(rojo2, LOW);
+  analogWrite(verde2, 0);
+  digitalWrite(rojo3, LOW);
+  analogWrite(verde3, 0);
+  
+  //
+  
+  analogWrite(verde3, 60);
+  analogWrite(azul3, 60);
+  delay(500);
+  analogWrite(verde3, 0);
+  analogWrite(azul3, 0);
+    
+  //
+  
+  digitalWrite(rojo1, HIGH);
+  analogWrite(verde1, 60);
+  digitalWrite(rojo2, HIGH);
+  analogWrite(verde2, 60);
+  digitalWrite(rojo3, HIGH);
+  analogWrite(verde3, 60);
+  delay(100);
+  digitalWrite(rojo1, LOW);
+  analogWrite(verde1, 0);
+  digitalWrite(rojo2, LOW);
+  analogWrite(verde2, 0);
+  digitalWrite(rojo3, LOW);
+  analogWrite(verde3, 0);
+  analogWrite(buzz, 255);
+  delay(100);
+  analogWrite(buzz, 0);
+  digitalWrite(rojo1, HIGH);
+  analogWrite(verde1, 60);
+  digitalWrite(rojo2, HIGH);
+  analogWrite(verde2, 60);
+  digitalWrite(rojo3, HIGH);
+  analogWrite(verde3, 60);
+  delay(100);
+  digitalWrite(rojo1, LOW);
+  analogWrite(verde1, 0);
+  digitalWrite(rojo2, LOW);
+  analogWrite(verde2, 0);
+  digitalWrite(rojo3, LOW);
+  analogWrite(verde3, 0);
+  delay(100);
+  digitalWrite(rojo1, HIGH);
+  analogWrite(verde1, 60);
+  digitalWrite(rojo2, HIGH);
+  analogWrite(verde2, 60);
+  digitalWrite(rojo3, HIGH);
+  analogWrite(verde3, 60);
+  delay(100);
+  digitalWrite(rojo1, LOW);
+  analogWrite(verde1, 0);
+  digitalWrite(rojo2, LOW);
+  analogWrite(verde2, 0);
+  digitalWrite(rojo3, LOW);
+  analogWrite(verde3, 0);
+  delay(100);
+  digitalWrite(rojo1, HIGH);
+  analogWrite(verde1, 60);
+  digitalWrite(rojo2, HIGH);
+  analogWrite(verde2, 60);
+  digitalWrite(rojo3, HIGH);
+  analogWrite(verde3, 60);
+  delay(100);
+  digitalWrite(rojo1, LOW);
+  analogWrite(verde1, 0);
+  digitalWrite(rojo2, LOW);
+  analogWrite(verde2, 0);
+  digitalWrite(rojo3, LOW);
+  analogWrite(verde3, 0);
+  
+  //
+  
+}
